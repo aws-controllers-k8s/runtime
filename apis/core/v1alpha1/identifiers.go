eeb621ff6d3a2800b0af923ac599b11c2757f403
Nicholas Thomson <nithomso@amazon.com>
2021-05-12 16:09:08 -0700

    Add additional keys to adopted resource spec

--- apis/core/v1alpha1/identifiers.go
+++ apis/core/v1alpha1/identifiers.go
@@ -21,6 +21,9 @@ type AWSIdentifiers struct {
 	// NameOrId is a user-supplied string identifier for the resource. It may
 	// or may not be globally unique, depending on the type of resource.
 	NameOrID *string `json:"nameOrID,omitempty"`
+	// AdditionalKeys represents any additional arbitrary identifiers used when
+	// describing the target resource.
+	AdditionalKeys map[string]*string `json:"additionalKeys,omitempty"`
 }
 
 // TargetKubernetesResource provides all the values necessary to identify a given ACK type
