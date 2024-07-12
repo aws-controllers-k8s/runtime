ebeff3574bfbd0bfd2e2e70f65e3f97b9f6913b6
Amine <hilalyamine@gmail.com>
2024-07-12 00:35:07 -0700

    Update runtime dependencies and toolchain (#150)
    This commit updates several Go module dependencies to their latest versions:
    
    - Update to Go 1.22.5 runtime and 1.22.3 toolchain
    - Update k8s.io dependencies to v0.30.0 (from v0.29.0)
     - `k8s.io/api`
     - `k8s.io/apimachinery`
     - `k8s.io/client-go`
     - `k8s.io/apiextensions-apiserver`
    - Update `controller-runtime` to v0.18.2 (from v0.17.2)
    - Update `klog` to v2.120.1 (from v2.110.1)
    
    it also adds a fake `AddMetricsServerExtraHandler` method to the
    fakeManager in the unit tests to satisfy the updated `ctrlruntime.Manager`
    interface
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- mocks/pkg/types/reconciler.go
+++ mocks/pkg/types/reconciler.go
@@ -64,6 +64,20 @@ func (_m *Reconciler) SecretValueFromReference(_a0 context.Context, _a1 *v1alpha
 	return r0, r1
 }
 
+// WriteToSecret provides a mock function with given fields: _a0, _a1, _a2, _a3, _a4
+func (_m *Reconciler) WriteToSecret(_a0 context.Context, _a1 string, _a2 string, _a3 string, _a4 string) error {
+	ret := _m.Called(_a0, _a1, _a2, _a3, _a4)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, string, string, string, string) error); ok {
+		r0 = rf(_a0, _a1, _a2, _a3, _a4)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // NewReconciler creates a new instance of Reconciler. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewReconciler(t interface {
